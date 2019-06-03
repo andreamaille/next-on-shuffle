4e5d48ccdc1bf34a2f06777d9e889147f622fd66
andreamaille <andrea.maille@gmail.com>
2019-06-03 00:16:17 -0400

    Updates

---
+++ precache-manifest.5db2bae99c2700dcd3a13f708688f438.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958eb6a0f1d1e8d79fb37a7a5f05b2d8",
+    "url": "/andrea-maille-project-five/index.html"
+  },
+  {
+    "revision": "11b8c11f5445aeb6ded6",
+    "url": "/andrea-maille-project-five/static/css/main.dd15aa86.chunk.css"
+  },
+  {
+    "revision": "88faf882753360c36c63",
+    "url": "/andrea-maille-project-five/static/js/2.d27cf5fa.chunk.js"
+  },
+  {
+    "revision": "11b8c11f5445aeb6ded6",
+    "url": "/andrea-maille-project-five/static/js/main.e9251ea4.chunk.js"
+  },
+  {
+    "revision": "946e4985f7af73c85f6b",
+    "url": "/andrea-maille-project-five/static/js/runtime~main.c5ae9c19.js"
+  },
+  {
+    "revision": "3b53c45c2cae0d94f76138bd592846db",
+    "url": "/andrea-maille-project-five/static/media/header-dark-image.3b53c45c.jpg"
+  },
+  {
+    "revision": "b3c34e04df6af585eb9881ca994536f0",
+    "url": "/andrea-maille-project-five/static/media/headphones-2.b3c34e04.jpg"
+  },
+  {
+    "revision": "b450b17d62d7939109e20be5d4525a2f",
+    "url": "/andrea-maille-project-five/static/media/headphones.b450b17d.jpg"
+  },
+  {
+    "revision": "793186731c472c13402b8dc937a12dd7",
+    "url": "/andrea-maille-project-five/static/media/ipod.79318673.jpg"
+  },
+  {
+    "revision": "997871e4e05965dfcc155d865abb5835",
+    "url": "/andrea-maille-project-five/static/media/listening-music.997871e4.jpg"
+  },
+  {
+    "revision": "9cef9b6f88a83089d4da9795ab3b802d",
+    "url": "/andrea-maille-project-five/static/media/mixtape.9cef9b6f.jpg"
+  }
+]);
